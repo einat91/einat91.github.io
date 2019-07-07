669fb6fe31dd33e6ce805156dcc06e99e935112b
einat91 <49745815+einat91@users.noreply.github.com>
2019-07-07 13:01:48 +0300

    spotify

--- einat91/jamming/src/util/Spotify.js
+++ einat91/jamming/src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = '3649dbca606a423ea621b98dc1d928a4';
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'https://git.heroku.com/einat-jamming.git/';
 let accessToken;
 
 const Spotify = {
