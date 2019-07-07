36549246521669bcd5584b8d5e18e4b6938d545c
einat91 <49745815+einat91@users.noreply.github.com>
2019-07-07 12:38:55 +0300

    changes

--- einat91/ravenous/src/Components/BusinessList/BusinesList.js
+++ einat91/ravenous/src/Components/BusinessList/BusinesList.js
@@ -4,10 +4,17 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
+    check() {
+        if (!this.props) {
+            return <h1> Oops! Something went wrong</h1>
+        }
+    }
+
     render() {
         return (
             <div className="BusinessList">
                 {
+                    //this.check() && 
                     this.props.businesses.map(business => {
                         return <Business business={business} key={business.id} />
                     })
--- einat91/ravenous/src/util/Yelp.js
+++ einat91/ravenous/src/util/Yelp.js
@@ -1,35 +1,49 @@
 
 const apiKey = 'TxeveKMsCP7bYixVUXuMZN499D_B5QwwHq9Xxs5qlQV0XRztFLhit-yKQpZezFOWNTiobzru75as1JyewDCQTNZhUw7db_n7fljk-iCrKnpvKRovetCRkiH6UUYZXXYx';
+const fetchAddress = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search`;
 
 const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        return fetch(`${fetchAddress}?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
-        }).then(response => {
-            if (!response.ok) {
-                throw Error('Error!');
-            } return response.json();
-        }).then(jsonResponse => {
-            if (jsonResponse) {
-                if (jsonResponse.businesses) {
-                    return jsonResponse.businesses.map(business => ({
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.location.address1,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code,
-                        category: business.categories[0].title,
-                        rating: business.rating,
-                        reviewCount: business.review_count
-                    }));
-                }
-            }
         })
+            .then(response => {
+                return response.json();
+            }).then(jsonResponse => {
+                if (jsonResponse) {
+                    if (jsonResponse.businesses) {
+                        return jsonResponse.businesses.map((business) => ({
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count
+                        }));
+                    }
+                }
+            })
+
+            .catch(response => {
+                if (response.status === 400) {
+                    alert('Error! Valid input is required');
+                   // return fetch(`${fetchAddress}?term=${term}&location=${location}&sort_by=${sortBy}`, {
+                     //   headers: {
+                       //     Authorization: `Bearer ${apiKey}`
+                        //}
+                    //})
+                }
+            })
     }
-};
+}
+
+
+
 
 export default Yelp;
