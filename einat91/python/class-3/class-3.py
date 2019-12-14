bc4bf006d38c816066172c0e0d79edea03aa0646
einat91 <49745815+einat91@users.noreply.github.com>
2019-12-14 23:34:17 +0200

    python

--- einat91/python/class-3/class-3.py
+++ einat91/python/class-3/class-3.py
@@ -1,3 +1,11 @@
+# import os
+# OS.func1(...)
+# from my_module import my_function1, my_function2
+# myfunction2(...)
+# import my_module as mm
+# mm.function3(...)
+
+# Regular Expressions
 # Regular Expressions match patterns in the text. They are more dynamic than searching for specific text.
 # They are useful for validating user input, to protect the system/website from a hacker attack.
 # (SQL Injection/ HTML Injection).
@@ -16,6 +24,15 @@
 # incompatible with each other and/or with the Python interpreter, so you can have the
 # specific versions of the packages you need in each enviornment and nothing "Breaks".
 
+
+# if _name_ == _main_:
+#   main()
+# The effect on this is that if the current script is being run directly,
+# the if condition is True and the main() function is executed.
+# If the script is imported and used by another module - the main () function
+# of the script is not executed unless called explicitly module_x.run().
+#
+
 # random
 # math
 # sys
@@ -26,4 +43,77 @@
 # if __name__ == __main__:
 # main()
 
+# def add_employee(emp, emp_list=None):
+#     if emp_list is None:
+#         emp_list = []
+#     emp_list.append(emp)
+#     print(emp_list)
 
+
+# emps = ['John', 'Jane']
+
+# add_employee('Corey', emps) # adding to an existed list
+# add_employee('Dan') # new list
+# add_employee('Ted') # appending it to the new list
+# Default arguments are evaluated once at the time it creates the function.
+# Mutable default argument:
+# emp_list=NONE, making sure it's empty.
+
+import Lib.glob
+import Lib.html
+import time
+from Lib.datetime import datetime
+
+
+def display_time(time=None):
+    if time is None:
+        time = datetime.now()
+    print(time.strftime('%B %D, %Y %H: %M %S'))
+
+
+display_time()
+time.sleep(1)
+display_time()
+time.sleep(1)
+display_time()
+
+names = ['Peter Parker', 'Bruce Wayne', 'Wade Wilson', 'Clark Kent']
+heroes = ['Spiderman', 'Batman', 'Deadpool', 'Superman']
+
+# Python 2 and 3 don't work at the same way
+
+identities = zip(names, heroes)
+print(identities)
+print(list(identities))  # Casint into a list
+# zip = zip up the first names in two lists (an iterator)
+
+for identity in identities:
+    print('{} is actualy {}!'.format(identity[0], identity[1]))
+
+# Aestriks = importing everying. Hard to debug and casues errors.
+
+print(Lib.html.escape)
+print(Lib.glob.escape)
+# Best way
+
+# Function parameters with Default Values
+
+def func_append(a, list1=[]):
+    list1.append(a)
+    print(list1)
+# This will not create an empty list1 parameter every time you call the function without
+# passing in a variable for list1. If you want an empty list in this scenario you should instead do:
+
+
+def func_append(a, list1=None):
+    if list1 is None:
+        list1 = []
+    list1.append(a)
+    print(list1)
+
+# Packages install: pip
+
+# Regular Expressions (Regexs)
+
+
+#
--- einat91/python/class-3/intro.py
+++ einat91/python/class-3/intro.py
@@ -1,4 +1,6 @@
 #import my_module as mm
+import re  # (command line)
+import Lib.antigravity
 from my_module import find_index, test
 import Lib.random as random
 import math
@@ -11,8 +13,8 @@ courses = ['History', 'Math', 'Physics', 'CompSci']
 
 #index = mm.find_index(courses, 'Math')
 index = find_index(courses, 'Math')
-#print(index) 
-#print(test)
+# print(index)
+# print(test)
 random_course = random.choice(courses)
 rads = math.radians(90)
 
@@ -23,9 +25,98 @@ today = datetime.date.today()
 print(today)
 print(calendar.isleap(2017))
 
-#print (os.getcwd()) # Current working directory
-#print(os.__file__) # Location of file in file system
+# print (os.getcwd()) # Current working directory
+# print(os.__file__) # Location of file in file system
 
-import Lib.antigravity
+
+# Debugging tools
+
+# Regular Expressions (Regexs)
+# Allow us to find a pattern in a specific text
+
+sentence = "My Phone number is 425-341-2591"
+phoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumberRegex(sentence)  # match ovject
+mo.group()  # returns the actual object
+
+# Grouping with parentheses.
+phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo.group(1)  # first group
+mo.group(2)  # second group
+mo.group()  # the whole expression
+
+fruitRegex = re.compile(r'strawberries|blueberries')
+mo = fruitRegex.search(sentence)
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')  # ? as *
+mo = phoneRegex.search('This is my number 123-456-7890')
+mo.group()  # '123-456-7890'
+mo = phoneRegex.search('This is mu number 456-7890')
+mo.group()  # 456-7890
+
+batRegex = re.compile(r'Bat(wo)*man')  # Batman, Batwomen, or any repetiotion
+mo = batRegex.search('I am Batwoman')
+mo.group()  # Batman
+mo = batRegex.search('I am Batwowowowoman')
+mo.group()  # Batwowowoman
+
+batRegex = re.compile(r'Bat(wo)?man')  # Batmen or Batwomen
+batRegex = re.compile(r'Bat(wo)+man')  # Batwoman or any repetiotion
+
+# Findall
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneRegex.findall('Amy is 425-214-3121 and Bob is 561-654-3121')
+# ['425-214-3121', '561-654-3121']
+phoneRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+phoneRegex.findall('Amy is 425-214-3121 and Bob is 561-654-3121')
+# [('425', '214', '3121'), ('561', '654', '3121')]
+# Tuples with three groups
+
+# \D means not a digit: spaces, underscores, letters
+# \w means alphanumeric: numbers, letters and underscores
+# \W means the opposite, non alphanumeric
+# \s means white space character: newline, space, tab, return or form
+# + one character or more
+
+regex = re.compile(r'\d+\s\w+')
+regex.findall('12 peaches, 15 lemons and 14 cherries')
+
+# Creating a class: vowels
+vowelRegex = re.compile(r'[AEIOUaeiou]')
+vowelRegex.findall('This is a sentence')
+# ['i', 'i', 'a', 'e', 'e', 'e']
+
+# Carrot: Beginning of a sentence ^ 
+# Dollar: End of a sentence $
+
+# not including vowels
+vowelRegex = re.compile(r'[^AEIOUaeiou]')
+vowelRegex.findall('This is a sentence')
+# ['T', 'h', 's', ' ', 's', ' ', ' ', 's',
+
+helloRegex = re.compile(r'^Hello')
+mo = helloRegex.search('Hello this is bob')
+mo.group() # 'Hello'
+
+# raw strings: r'str': ne need to escape the meta characters in the string 
+# when writing r before the string
+# Meta characters have special meaning and need to be escaped with backslased \t \s.
+# When reading text from a file, and testing a pattern on it you could get an encoding error. 
+# Fix by using encoding ='utf-8' when reading the file.
+# Greedy (default) = matches as much as possible: * or +
+
+# Cheat Sheet
+# (?aiLmsux): one or more of the letters can be used as flag(s), e.g: (?i)(pattern) matches
+# lower case and upper or a mixture of the pattern which follows.
+# The split() and sub() functions.
+# \b, a character class which matches a word boundary, is useful
+# {m, n} for when you want to have the matches of the character or group between two nums
+# finditer(): (not in the cheat sheet), iterates the matches: for match in finditer(pattern,str):
+
+# Method for Easily Writing Regex
+# If your regex doesn't match any part of the text you are working on, try using only a part of
+# the pattern you want, or using a more general pattern - when you get more maches,
+# you can see how to add more parts/ more specifity into the pattern, testing the pattern with each change.
+
+# Python Regular Expressions tips
 
 
